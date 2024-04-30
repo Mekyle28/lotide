fabb864b210a351e95c18ee82dff7020ca7ded98
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-04-29 23:54:06 -0400

    assertEqual function added

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🔴🔴🔴Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 10);
+assertEqual(100, 100);
+assertEqual("HI", "HI");
+assertEqual("HI", "hi");
