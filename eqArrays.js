0e5739e7b56e43772c16b1dcb0f2cc5bcc8f8948
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-04-30 23:29:11 -0400

    test prompts updated

--- eqArrays.js
+++ eqArrays.js
@@ -17,12 +17,10 @@ const eqArrays = function(arr1, arr2) {
 
 // test case
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 6, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 6, 3]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
 assertEqual(eqArrays([1, 2, 3, 5, 10, 100], [1, 2, 3, 5, 10, 100]), true);
 assertEqual(eqArrays([], []), true);
 assertEqual(eqArrays(["the", "dog" ], ["the", "dog"]), true);
-assertEqual(eqArrays(["the", "dog" ], ["the", "cat"]), true);
-
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result.join(", "), ["Lighthouse", "Labs"].join(", "));
+assertEqual(eqArrays(["the", "dog" ], ["the", "cat"]), false);
+assertEqual(eqArrays([1, 23], [12, 3]), false);
