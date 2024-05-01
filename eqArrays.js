dfdb2965e0b8fb8af7c69777e013cc5423837d7b
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-01 00:14:41 -0400

    debugg

--- eqArrays.js
+++ eqArrays.js
@@ -7,16 +7,18 @@ const assertEqual = function(actual, expected) {
 };
   
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.lenght; i++)
-      if (arr1[i] === arr2[1]) {
-        return true;
-      }
+  if (arr1.length !== arr2.length) {
+    return false;
   }
-  return false;
+  for (let i = 0; i < arr1.length; i++)
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  
+  return true;
 };
 
-// my first attempt that converted them to strings to compare
+// //my first attempt that converted them to strings to compare
 // const eqArrays = function(arr1, arr2) {
 //   if (arr1.join(", ") === arr2.join(", ")) {
 //     return true;
