f3f04103c40e11c7245faacb646eb4fc7fa45550
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-02 11:48:14 -0400

    initialized

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,16 @@
+// this function checks if two arrays are equal
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++)
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  
+  return true;
+};
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -12,14 +25,36 @@ const eqObjects = function(object1, object2) {
     return false;
   }
   for (let key in object1)
-    if (object1[key] !== object2[key]) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (eqArrays(object1[key], object2[key]) !== true) {
+        return false;
+      }
+    } if (object1[key] !== object2[key]) {
       return false;
     }
   return true;
 };
 
+// // function that compares two objects
+// const eqObjects = function(object1, object2) {
+//   let object1Arr = Object.keys(object1);
+//   let object2Arr = Object.keys(object2);
+//   if (object1Arr.length !== object2Arr.length) {
+//     return false;
+//   }
+//   for (let key of object1Arr)
+//   if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+//     if (eqArrays(object1[key], object2[key]) !== true) {
+//       return false;
+//     }
+//   }if (object1Arr[key] !== object2Arr[key]) {
+//       return false;
+//     }
+//   return true;
+// };
 
-// test case
+
+// test case with primatives as values
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
 assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
@@ -28,3 +63,14 @@ assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
 const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
 assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
 
+// test case with Arrays
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+eqObjects(multiColorShirtObject, anotherMultiColorShirtObject); // => true
+
+const longSleeveMultiColorShirtObject = {
+  size: "medium",
+  colors: ["red", "blue"],
+  sleeveLength: "long",
+};
+eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject); // => false
