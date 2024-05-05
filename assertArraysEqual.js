16611e89ca13372614372e1b866e7eb704aaf08c
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-05 11:07:10 -0400

    test case fixed

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,4 @@
-// const eqArrays = function(arr1, arr2) {
-//   if (arr1.join(", ") === arr2.join(", ")) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-  
-
+// this function checks if two arrays are equal
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -15,26 +7,28 @@ const eqArrays = function(arr1, arr2) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
-    
+      
   return true;
 };
 
-const assertArraysEqual = function(result) {
-  if (result === true) {
-    console.log(`The arrays are equal`);
+// this function checks if two arrays are equal and logs the result.
+const assertArraysEqual = function(arrActual, arrExpected) {
+  if (eqArrays(arrActual, arrExpected)) {
+    console.log(`✅The arrays [${arrActual}] and [${arrExpected}] are equal`);
   } else {
-    console.log(`The arrays are not equal`);
+    console.log(`🔴The arrays [${arrActual}] and [${arrExpected}] are not equal`);
   }
 };
 
+
 // test cases
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3])); // are equal
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // are equal
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2])); // not equal
+assertArraysEqual([1, 2, 3], [1, 2]); // not equal
 
-assertArraysEqual(eqArrays([], [])); // equal
+assertArraysEqual([], []); // equal
 
-assertArraysEqual(eqArrays(["i'm", "so", "tired"], ["i'm", "so", "tired"])); // equal
+assertArraysEqual(["i'm", "so", "tired"], ["i'm", "so", "tired"]); // equal
 
-assertArraysEqual(eqArrays(["i'm", "so", "tired"], ["i'm", "so", "sleepy"])); // not equal
+assertArraysEqual(["i'm", "so", "tired"], ["i'm", "so", "sleepy"]); // not equal
 
