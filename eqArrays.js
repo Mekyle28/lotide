10ba74c7253d0e111a6768e38881fc14422a9f47
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-06 16:02:17 -0400

    moved tests to eqArraysTest.js, format, export

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,4 @@
-const assertEqual = require("/home/labber/lighthouse/lotide/assertEqual");
-
-const eqArrays = function (arr1, arr2) {
+const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
     return false;
   }
@@ -11,27 +9,4 @@ const eqArrays = function (arr1, arr2) {
   return true;
 };
 
-// test case
-console.log("eqArrays test");
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 6, 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
-assertEqual(eqArrays([1, 2, 3, 5, 10, 100], [1, 2, 3, 5, 10, 100]), true);
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays(["the", "dog"], ["the", "dog"]), true);
-assertEqual(eqArrays(["the", "dog"], ["the", "cat"]), false);
-assertEqual(eqArrays([1, 23], [12, 3]), false);
-console.log("-----------");
-
 module.exports = eqArrays;
-// paste this into file that you want to use this function.
-// const eqArrays = require("/home/labber/lighthouse/lotide/eqArrays"); 
-
-// //my first attempt that converted them to strings to compare
-// const eqArrays = function(arr1, arr2) {
-//   if (arr1.join(", ") === arr2.join(", ")) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
