d1281feb8e2ddc44c48637e82594f7119a88921e
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-06 00:04:55 -0400

    refactor, add export and require

--- eqObjects.js
+++ eqObjects.js
@@ -1,32 +1,15 @@
-// this function checks if two arrays are equal
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++)
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  
-  return true;
-};
+const eqArrays = require("/home/labber/lighthouse/lotide/eqArrays"); 
+const assertEqual = require("/home/labber/lighthouse/lotide/assertEqual");
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
-// function that compares two objects
+// function that compares two objects and returns true if equal or false if not
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
   for (let key in object1) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (eqArrays(object1[key], object2[key]) !== true) {
+      if (eqArrays(object1[key], object2[key]) !== true) {   // ??change to this: (!eqArrays(object1[key], object2[key]))
         return false;
       }
     } else {
@@ -37,6 +20,41 @@ const eqObjects = function(object1, object2) {
   }
   return true;
 };
+
+
+
+// test case with primatives as values
+console.log("eqObjects test: objects with primatives as values");
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
+
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
+console.log("--------------");
+// test case with Arrays
+console.log("eqObjects test: objects with Arrays as values");
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true); // => true
+
+const longSleeveMultiColorShirtObject = {
+  size: "medium",
+  colors: ["red", "blue"],
+  sleeveLength: "long",
+};
+assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false); // => false
+console.log("--------------");
+
+module.exports = eqObjects;
+// paste this into file to use this function 
+const eqArrays = require("/home/labber/lighthouse/lotide/eqObjects"); 
+
+
+
+
+
 
 // // function that compares two objects changes objects to arrays
 // const eqObjects = function(object1, object2) {
@@ -58,25 +76,3 @@ const eqObjects = function(object1, object2) {
 // }
 // return true;
 // };
-
-
-// test case with primatives as values
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject = { size: "medium", color: "red" };
-assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
-
-
-const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
-assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
-
-// test case with Arrays
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true); // => true
-
-const longSleeveMultiColorShirtObject = {
-  size: "medium",
-  colors: ["red", "blue"],
-  sleeveLength: "long",
-};
-assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false); // => false
