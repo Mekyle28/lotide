b7f9098b9c6c45f4d3ca0ca4d31d945700a43a21
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-05 23:47:31 -0400

    refactor add require

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-  
-const eqArrays = function(arr1, arr2) {
+const assertEqual = require("/home/labber/lighthouse/lotide/assertEqual");
+
+const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
@@ -14,9 +8,22 @@ const eqArrays = function(arr1, arr2) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
-  
   return true;
 };
+
+// test case
+console.log("eqArrays test");
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 6, 3]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
+assertEqual(eqArrays([1, 2, 3, 5, 10, 100], [1, 2, 3, 5, 10, 100]), true);
+assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays(["the", "dog"], ["the", "dog"]), true);
+assertEqual(eqArrays(["the", "dog"], ["the", "cat"]), false);
+assertEqual(eqArrays([1, 23], [12, 3]), false);
+console.log("-----------");
+
+module.exports = eqArrays;
 
 // //my first attempt that converted them to strings to compare
 // const eqArrays = function(arr1, arr2) {
@@ -26,14 +33,3 @@ const eqArrays = function(arr1, arr2) {
 //     return false;
 //   }
 // };
-
-
-// test case
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 6, 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
-assertEqual(eqArrays([1, 2, 3, 5, 10, 100], [1, 2, 3, 5, 10, 100]), true);
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays(["the", "dog" ], ["the", "dog"]), true);
-assertEqual(eqArrays(["the", "dog" ], ["the", "cat"]), false);
-assertEqual(eqArrays([1, 23], [12, 3]), false);
