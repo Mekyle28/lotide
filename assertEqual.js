fb8c609111128554e27b21125e08d81d33fd972a
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-05 23:36:21 -0400

    refactor and add export

--- assertEqual.js
+++ assertEqual.js
@@ -1,16 +1,25 @@
+
+// compares either two integers or strings and prints if they are equal.
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
+    return console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
 };
+module.exports = assertEqual;
 
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 10);
-assertEqual(100, 100);
-assertEqual("HI", "HI");
-assertEqual("HI", "hi");
+// TEST CODE 
+console.log("assertEqual test code:");
+assertEqual("Lighthouse Labs", "Bootcamp"); //should fail
+assertEqual(1, 1); // should pass
+assertEqual(1, 10); //should fail
+assertEqual(100, 100); // should pass
+assertEqual("HI", "HI"); // should pass
+assertEqual("HI", "hi"); //should fail
+console.log( "---------");
+
+
+
+// paste this into file to use this function 
+// const assertEqual = require("/home/labber/lighthouse/lotide/assertEqual");
