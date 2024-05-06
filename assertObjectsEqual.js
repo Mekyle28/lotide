3f01ad04e325daac8ef1039c6a1ece05de280f01
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-06 00:19:49 -0400

    refactor, export and require added

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,21 +1,4 @@
-// function that compares two objects
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  }
-  for (let key in object1) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (eqArrays(object1[key], object2[key]) !== true) {   // ??change to this: (!eqArrays(object1[key], object2[key]))
-        return false;
-      }
-    } else {
-      if (object1[key] !== object2[key]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
+const eqObjects = require("/home/labber/lighthouse/lotide/eqObjects"); 
 
 
 // function will take in two objects and
@@ -30,7 +13,14 @@ const assertObjectsEqual = function (actual, expected) {
   };
 
   
-
+// tests 
+console.log("assertobjectsEqual tests:");
   assertObjectsEqual({ a: 1, b: 2, c: 3 }, { a: 1, b: 2, c: 3 }); // should pass
   assertObjectsEqual({ a: 1, b: 2, c: 3 }, { a: 6, b: 2, c: 3 }); // should fail 
   assertObjectsEqual({}, {}); // should pass
+  console.log("-----------");
+
+  module.exports = assertObjectsEqual;
+  // paste this into file to use this function 
+  //const assertObjectsEqual = require("/home/labber/lighthouse/lotide/assertObjectsEqual"); 
+  
