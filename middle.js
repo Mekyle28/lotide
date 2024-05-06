48db6cfafde2047e7ca8935690339daab02d29bd
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-06 18:56:21 -0400

    test removed, refactor

--- middle.js
+++ middle.js
@@ -1,47 +1,19 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++)
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-      
-  return true;
-};
-  
-
-// this function checks if two arrays are equal and logs the result.
-const assertArraysEqual = function(arrActual, arrExpected) {
-  if (eqArrays(arrActual, arrExpected) === true) {
-    console.log(`✅The arrays [${arrActual}] and [${arrExpected}] are equal`);
-  } else {
-    console.log(`🔴The arrays [${arrActual}] and [${arrExpected}] are not equal`);
-  }
-};
-
-  // function will take in an array and return the middle-most element(s) of the given array
-const middle = function(array) {
+// function will take in an array and return the middle-most element(s) of the given array
+const middle = function (array) {
   let middleArr = [];
-  let num = 0;
+  let middleIndex = Math.floor(array.length / 2);
+
   if (array.length > 2) {
+    // we can return the middle for an array of 1 || 2
     if (array.length % 2 !== 0) {
-      num = Math.floor(array.length / 2);
-      middleArr.push(array[num]);
-    } else {
-      num = (array.length / 2);
-      middleArr.push(array[num - 1]);
-      middleArr.push(array[num]);
+      middleArr.push(array[middleIndex]);
+    } 
+    if (array.length % 2 === 0) {
+      middleArr.push(array[middleIndex - 1]);
+      middleArr.push(array[middleIndex]);
     }
   }
   return middleArr;
 };
-  
 
-// test code
-
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-
-assertArraysEqual(middle([1, 2]), []);
+module.exports = middle;
