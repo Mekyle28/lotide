15656509e1b2fa4f6bede726ae4764b98dac1d6c
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-06 01:08:34 -0400

    require added, format, refactor

--- letterPositions.js
+++ letterPositions.js
@@ -1,51 +1,28 @@
-// this function checks if two arrays are equal
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++)
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-      
-  return true;
-};
-
-// this function checks if two arrays are equal and logs the result.
-const assertArraysEqual = function(arrActual, arrExpected) {
-  if (eqArrays(arrActual, arrExpected) === true) {
-    console.log(`✅The arrays [${arrActual}] and [${arrExpected}] are equal`);
-  } else {
-    console.log(`🔴The arrays [${arrActual}] and [${arrExpected}] are not equal`);
-  }
-};
+const assertArraysEqual = require("/home/labber/lighthouse/lotide/assertArraysEqual");
+const eqArrays = require("/home/labber/lighthouse/lotide/eqArrays");
 
 // function takes a string and returns an object with the
 // index value for each letter in the string
-const letterPositions = function(sentence) {
+const letterPositions = (string) => {
   const results = {};
-  for (let i = 0; i < sentence.length; i++) {
-    if (sentence[i] !== " ") {
-      if (results[sentence[i]]) {
-        results[sentence[i]].push(i);
-      } else {
-        results[sentence[i]] = [i];
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] !== " ") {
+      if (!results[string[i]]) {
+        results[string[i]] = [];
       }
+      results[string[i]].push(i);
     }
   }
   return results;
 };
 
 //test code
-console.log((letterPositions("")));
+console.log("letterPositions test:");
+console.log(letterPositions(""));
 
-let object = (letterPositions("hello"));
+let object = letterPositions("hello");
 
 assertArraysEqual(object["h"], [0]);
 assertArraysEqual(object["e"], [1]);
 assertArraysEqual(object["l"], [2, 3]);
 assertArraysEqual(object["o"], [4]);
-
-
-
-
