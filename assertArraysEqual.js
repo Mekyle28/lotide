57a279c8b1cc82d514dcc21c82a57ae226817fe2
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-06 18:34:37 -0400

    tests moved to test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -9,21 +9,4 @@ const assertArraysEqual = function(arrActual, arrExpected) {
   }
 };
 
-
-// // test cases
-console.log("assertArraysEqual tests: ");
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // are equal
-
-assertArraysEqual([1, 2, 3], [1, 2]); // not equal
-
-assertArraysEqual([], []); // equal
-
-assertArraysEqual(["i'm", "so", "tired"], ["i'm", "so", "tired"]); // equal
-
-assertArraysEqual(["i'm", "so", "tired"], ["i'm", "so", "sleepy"]); // not equal
-console.log("------------");
-
-
 module.exports = assertArraysEqual;
-// paste this into file to use this function 
-//const eqArrays = require("/home/labber/lighthouse/lotide/assertArraysEqual"); 
