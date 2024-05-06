33fa2f72df260148f0e8be856bbb0e7ac5c1498e
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-06 00:28:42 -0400

    require added, formated, // added

--- countLetters.js
+++ countLetters.js
@@ -1,35 +1,28 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("/home/labber/lighthouse/lotide/assertEqual");
 
-// function counts how many times a letter shows up in a string and returns as an object
-const countLetters = function(string) {
-  let results = {};
-  let stringNoSpaces = string.split(" ").join(""); // removes the spaces
+// function counts how many times a letter shows up in a string and returns an object
+const countLetters = function (string) {
+  const results = {};
+  const stringNoSpaces = string.split(" ").join(""); // removes the spaces
   for (let letter of stringNoSpaces) {
-    if (results[letter]) {
-      results[letter] += 1;   // Increses the letter count by 1 if the letter is already in the object
-    } else {
-      results[letter] = 1;   //adds letter to the object
+    if (!results[letter]) {
+      results[letter] = 0; // Increases the letter count by 1 if the letter is already in the object
     }
+    results[letter]++; //adds letter to the object
   }
   return results;
 };
 
 // test code
+console.log("countLetters test: object (letters-numbers)");
 console.log(countLetters("I really hope this works"));
 console.log(countLetters("watermelon"));
 console.log(countLetters(""));
 
-
+console.log("countLetters test: assert=");
 let result2 = countLetters("A cat");
 
 assertEqual(result2["a"], 1);
 assertEqual(result2["c"], 1);
 assertEqual(result2["t"], 1);
 assertEqual(result2["A"], 1);
-
