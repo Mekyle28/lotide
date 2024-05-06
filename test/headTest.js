c2fcc286139a7cc330ab96287bffd596dd414eca
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-06 19:16:33 -0400

    changed test to mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,14 +1,25 @@
-const head = require("/home/labber/lighthouse/lotide/head");
-const assertEqual = require("/home/labber/lighthouse/lotide/assertEqual");
+const assert = require('chai').assert;
+const head   = require('../head');
 
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
 
-// test
-console.log("head.js tests");
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([10, "hi", "100"]), 10);
-assertEqual(head([10]), 10);
-assertEqual(head([]),);
-console.log("-----------");
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello'); 
+  });
+  it("returns '10' for ['10', 'hi', '100']", () => {
+    assert.strictEqual(head([10, "hi", "100"]), 10); 
+  });
+
+  it("returns 'undefined' for []", () => {
+    assert.strictEqual(head([]), ); 
+  });
+});
+
 
