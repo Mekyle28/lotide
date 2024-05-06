0f3dfd8a3e6d2780a75217dbb75503cdaac112ba
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-06 11:53:22 -0400

    test moved to test file

--- assertEqual.js
+++ assertEqual.js
@@ -9,16 +9,6 @@ const assertEqual = function(actual, expected) {
 module.exports = assertEqual;
 
 
-// TEST CODE 
-console.log("assertEqual test code:");
-assertEqual("Lighthouse Labs", "Bootcamp"); //should fail
-assertEqual(1, 1); // should pass
-assertEqual(1, 10); //should fail
-assertEqual(100, 100); // should pass
-assertEqual("HI", "HI"); // should pass
-assertEqual("HI", "hi"); //should fail
-console.log( "---------");
-
 
 
 // paste this into file to use this function 
