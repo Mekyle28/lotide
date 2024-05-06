95d7a6d8aec3521045e18f5812eb499cbd903614
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-06 18:55:39 -0400

    initialized, test added

---
+++ test/middleTest.js
@@ -0,0 +1,10 @@
+const assertArraysEqual = require("../assertArraysEqual");
+const middle = require("../middle");
+
+// test code
+
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+assertArraysEqual(middle([1, 2]), []);
