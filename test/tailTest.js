6345d83e9d2747f26ce26423683da508eac80c7b
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-06 15:54:12 -0400

    added more testing to check original array

--- test/tailTest.js
+++ test/tailTest.js
@@ -5,9 +5,10 @@ const tail = require("../tail");
 
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
+const wordsCopy = words;
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
-
+assertEqual(words, wordsCopy);
 // //test case 1
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); //ensure we get back two elements
