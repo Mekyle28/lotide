b89d64acdc5ea20096ebb64864e4aa1dfcf45e17
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-06 11:53:01 -0400

    test moved to test file

--- head.js
+++ head.js
@@ -5,13 +5,4 @@ const head = function(array) {
   return array[0];
 };
 
-
-
-// test
-console.log("head.js tests");
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([10, "hi", "100"]), 10);
-assertEqual(head([10]), 10);
-assertEqual(head([]),);
-console.log("-----------");
+module.exports = head;
