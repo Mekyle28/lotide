ac9f49601a4cca68feb2cac5f920545ad62660cb
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-05 23:39:18 -0400

    refactor add require

--- head.js
+++ head.js
@@ -1,10 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("/home/labber/lighthouse/lotide/assertEqual"); 
+
 
 const head = function(array) {
   return array[0];
@@ -13,8 +8,10 @@ const head = function(array) {
 
 
 // test
+console.log("head.js tests");
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([10, "hi", "100"]), 10);
 assertEqual(head([10]), 10);
 assertEqual(head([]),);
+console.log("-----------");
