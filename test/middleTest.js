be895ff23d5c7a28f162329753858180f9365c1d
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-06 20:33:28 -0400

    changed testing to chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,24 @@
 const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
+const assert = require('chai').assert;
 
-// test code
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#middle()", () => {
+  
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
 
-assertArraysEqual(middle([1, 2]), []);
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+  
+  it("returns ['r'] for ['hi', 'how', 'r', 'u', '?']", () => {
+    assert.deepEqual(middle(['hi', 'how', 'r', 'u', '?']), ['r']);
+  });
+});
