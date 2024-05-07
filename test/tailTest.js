201759ba9f0718535656e38a334f25852134b256
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-06 20:34:19 -0400

    changed testing to chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,28 @@
-const assertEqual = require("../assertEqual");
 const tail = require("../tail");
+const assert = require('chai').assert;
 
+const testArray = ["Yo Yo", "Lighthouse", "Labs"]
 
+describe("#tail", () => {
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const wordsCopy = words;
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(words, wordsCopy);
-// //test case 1
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); //ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure the first element is "Lighthouse"
-assertEqual(result[1], "Labs"); //ensure second element is "Labs"
+  
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo, 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(testArray), ['Lighthouse', 'Labs']);
+  });
+
+  it("check and make sure tail() didn't alter the original Array returns ['Yo Yo', 'Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(testArray, [ 'Yo Yo', 'Lighthouse', 'Labs' ]); 
+  });
+
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns ['hi', 100] for ['10', 'hi', '100']", () => {
+    assert.deepEqual(tail([10, "hi", 100]), ["hi", 100]); 
+  });
+
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]),[]); 
+  });
+});
